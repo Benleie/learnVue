47ad1f8e6862d2a8e63703f5ba18e9d239b0e2f1
ben leie <awecg@qq.com>
2020-08-13 21:27:38 +0800

    ctrl + f5   jsx

---
+++ learnReact/docs/src/create-a-complex-jsx-element.js
@@ -0,0 +1,16 @@
+const JSX = (
+    <div>
+        <h1>hello</h1>
+        <p>boys</p>
+        <ul>
+            <li>arya</li>
+            <li>sansa</li>
+            <li>bran</li>
+        </ul>
+    </div>
+)
+
+ReactDOM.render(
+    JSX,
+    document.getElementById('root')
+);
---
+++ learnReact/docs/src/create-a-simple-jsx-element.js
@@ -0,0 +1,9 @@
+/* 
+    * CTRL + F5
+ */
+const JSX = <h1>Hello JSX!</h1>;
+
+ReactDOM.render(
+    JSX,
+    document.getElementById('root')
+);
